933192c14f5d62fae43550ee3e9c080651359aca
joscha <joscha@.(none)>
2011-07-18 00:09:42 +0200

    unstable

--- js/main.js
+++ js/main.js
@@ -2,52 +2,38 @@ $(document).ready(function() {
 	var target = "http://www.wetterzentrale.de/topkarten/tkgfsmeur.htm?was=3&wann=00";
 	$.get(target, function(res) { //get the html source of this website
 		var array = $(res.responseText).next().next().next().next().next().last().find("a");
-		var links = [];
 		jQuery.each(array, function(index, value){
 			var link = "http://www.wetterzentrale.de" + value.toString().split('.')[4] + ".gif";
-			links.push({image: link, thumb : link, title : ""});
-	});
+		      $('#photos').append("<li><a>href=\"" + link + "\"</a></li>");
+		});
 
 	
-	jQuery(function($){
-		
-		$.supersized({
-		
-			// Functionality
-			slideshow               :   1,			// Slideshow on/off
-			autoplay				:	1,			// Slideshow starts playing automatically
-			start_slide             :   1,			// Start slide (0 is random)
-			stop_loop				:	0,			// Pauses slideshow on last slide
-			random					: 	0,			// Randomize slide order (Ignores start slide)
-			slide_interval          :   3000,		// Length between transitions
-			transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-			transition_speed		:	1000,		// Speed of transition
-			new_window				:	1,			// Image links open in new window/tab
-			pause_hover             :   0,			// Pause slideshow on hover
-			keyboard_nav            :   1,			// Keyboard navigation on/off
-			performance				:	1,			// 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
-			image_protect			:	1,			// Disables image dragging and right click with Javascript
-													   
-			// Size & Position						   
-			min_width		        :   0,			// Min width allowed (in pixels)
-			min_height		        :   0,			// Min height allowed (in pixels)
-			vertical_center         :   1,			// Vertically center background
-			horizontal_center       :   1,			// Horizontally center background
-			fit_always				:	0,			// Image will never exceed browser width or height (Ignores min. dimensions)
-			fit_portrait         	:   1,			// Portrait images will not exceed browser height
-			fit_landscape			:   0,			// Landscape images will not exceed browser width
-													   
-			// Components				
-			slide_links				:	'blank',	// Individual links for each slide (Options: false, 'number', 'name', 'blank')
-			thumb_links				:	1,			// Individual thumb links for each slide
-			thumbnail_navigation    :   0,			// Thumbnail navigation
-			slides 					:   links,
-										
-			// Theme Options			   
-			progress_bar			:	1,			// Timer for each slide							
-			mouse_scrub				:	0
-			
-		});
-    });
+		var _h = [], _w = [];
+		_h[0] =  300;//only for test
+        _w[0] =  800;//only for test
+        _h[1] =  300;//only for test
+        _w[1] =  800;//only for test
+        
+        var _o = {fullscreen: false, menu : "slider", style : "centered"}; 
+        var obj = jQuery(".jbgallery");
+              
+        function _init(opt){
+//			obj.eq(0).jbgallery("destroy");//NO CHINABILITY + API
+//			obj.eq(1).jbgallery("destroy");//NO CHINABILITY + API
+//            obj.eq(0).resizable("destroy");//NO CHINABILITY + API
+//            obj.eq(1).resizable("destroy");//NO CHINABILITY + API
+            
+			$('.jbgallery')
+			.each(function(i){
+				var self = this;
+				$(this).css({
+                    width: _w[i],
+                    height : _h[i]
+                })
+                .jbgallery(opt);
+			});
+        }
+        _init(_o);
+       
+	});
   });
-});
