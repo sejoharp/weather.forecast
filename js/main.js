ab5db5c264fafbc03fe9eae9bafd4005024afe4e
joscha <joscha@.(none)>
2011-07-19 13:29:25 +0200

    js design changed

--- js/main.js
+++ js/main.js
@@ -2,51 +2,55 @@ $(document)
 		.ready(
 				function() {
 					var target = "http://www.wetterzentrale.de/topkarten/tkgfsmeur.htm?was=3&wann=00";
-					$.get(target, function(res) {
-						var array = $(res.responseText).next().next().next()
-								.next().next().last().find("a");
-
-						jQuery.each(array, function(index, value) {
-							var link = "http://www.wetterzentrale.de"
-									+ value.toString().split('.')[4] + ".gif";
-							$('#gallery').append("<a href=\""+ link +"\"><img title=\"\" alt=\"\" src=\""+ link + "\"></a>");
-						});
-
-						Galleria.loadTheme('js/galleria.classic.js');
-						$("#gallery").galleria({
-							width : 820,
-							height : 740,
-							maxScaleRatio: 1,
-							minScaleRatio: 1,
-							showCounter: false,
-							showImagenav: false,
-							transition: "fade",
-							transitionSpeed: 1000,
-							extend: function() {
-						        var gallery = this;
-								$('#next').click(function() {
-									gallery.next();
-									console.log("klick on next");
-									});
-								$('#previous').click(function() {
-									gallery.prev();
-									});
-								$('#toggle').click(function() {
-									var button = $("#toggle");
-									if (button.text() == "play"){
-										gallery.play(2000);
-										button.text("stop");
-									}else{
-										gallery.pause();
-										button.text("play");
-									}
-									});
-						    }
-
-						});
-						
-
-					});
+					show_gallery(target, "#rainfall-gallery", "#rainfall-next", "#rainfall-previous", "#rainfall-toggle");
 				});
 
+function show_gallery(target, gallery, next, previous, toggle){
+	$.get(target, function(res) {
+		var array = $(res.responseText).next().next().next()
+				.next().next().last().find("a");
+		add_links(gallery, array);
+		init_galleria(gallery, next, previous, toggle);
+	});
+}
 
+function add_links(gallery, array){
+	jQuery.each(array, function(index, value) {
+		var link = "http://www.wetterzentrale.de"
+				+ value.toString().split('.')[4] + ".gif";
+		$(gallery).append("<a href=\""+ link +"\"><img title=\"\" alt=\"\" src=\""+ link + "\"></a>");
+	});	
+}
+function init_galleria(gallery, next, previous, toggle){
+	Galleria.loadTheme('js/galleria.classic.js');
+	$(gallery).galleria({
+		width : 820,
+		height : 740,
+		maxScaleRatio: 1,
+		minScaleRatio: 1,
+		showCounter: false,
+		showImagenav: false,
+		transition: "fade",
+		transitionSpeed: 1000,
+		extend: function() {
+	        var gallery = this;
+			$(next).click(function() {
+				gallery.next();
+				});
+			$(previous).click(function() {
+				gallery.prev();
+				});
+			$(toggle).click(function() {
+				var button = $(toggle);
+				if (button.text() == "play"){
+					gallery.play(2000);
+					button.text("stop");
+				}else{
+					gallery.pause();
+					button.text("play");
+				}
+				});
+	    }
+
+	});
+}
