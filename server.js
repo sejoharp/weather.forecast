013e741ca7f123f671d0de6213dc57ec724703a7
joscha <joscha@.(none)>
2011-07-30 10:54:51 +0200

    serverside link parsing

--- server.js
+++ server.js
@@ -1,47 +1,43 @@
-var sys = require('sys'),
-    http = require('http');
+var sys = require('sys'), http = require('http');
 
-	
 /*
-var connection = http.createClient(80, 'www.wetterzentrale.de'),
-    request = connection.request("GET",'/topkarten/tkgfsmeur.htm?was=3&wann=00');
-
-connection.addListener('error', function(connectionException){
-    sys.log(connectionException);
-});
-
-request.addListener('response', function(response){
-    var data = '';
-
-    response.addListener('data', function(chunk){ 
-        data += chunk; 
-    });
-    response.addListener('end', function(){
-        console.log(data);
-    });
-});
-
-request.end();
-*/
+ * var connection = http.createClient(80, 'www.wetterzentrale.de'), request =
+ * connection.request("GET",'/topkarten/tkgfsmeur.htm?was=3&wann=00');
+ * 
+ * connection.addListener('error', function(connectionException){
+ * sys.log(connectionException); });
+ * 
+ * request.addListener('response', function(response){ var data = '';
+ * 
+ * response.addListener('data', function(chunk){ data += chunk; });
+ * response.addListener('end', function(){ console.log(data); }); });
+ * 
+ * request.end();
+ */
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:1337/');
 
 var options = {
-  host: 'www.wetterzentrale.de',
-  port: 80,
-  path: '/topkarten/tkgfsmeur.htm?was=3&wann=00',
+	host : 'www.wetterzentrale.de',
+	port : 80,
+	path : '/topkarten/tkgfsmeur.htm?was=3&wann=00',
 };
 var data = "";
 
 var req = http.get(options, function(res) {
-  res.setEncoding('utf8');
-  res.on('data', function (chunk) {
-	data += chunk; 
-  });
-  res.on('end', function (chunk) {
-	console.log("fertig geladen");
-    console.log(chunk);
-  });
-});
-
-req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
+	res.setEncoding('utf8');
+	res.on('data', function(chunk) {
+		data += chunk;
+	});
+	res.on('end', function(chunk) {
+		console.log("fertig geladen");
+		console.log(data);
+	});
+	res.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+	});
 });
