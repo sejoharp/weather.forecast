34ed3fdff739a25eca079aa29b193980716dda87
joscha <joscha@ubuntu.(none)>
2011-08-14 22:51:26 -0700

    location changed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express.createServer();
 app.use("/js", express.static(__dirname + '/web/js'));
 app.use("/img", express.static(__dirname + '/web/img'));
 app.use("/css", express.static(__dirname + '/web/css'));
-app.get('/', function(req, res){
+app.get('/weather', function(req, res){
 	res.sendfile('web/weather.forecast.html');
 });
 app.listen(process.env.PORT || 8001);
